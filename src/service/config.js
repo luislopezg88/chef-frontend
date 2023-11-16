c75616e4e6ea018c08aa8104c4200912976f9fe1
Jorge C <jorgecolmenarez89@gmail.com>
2023-11-15 20:07:43 -0400

    ajustar url base del api

--- src/service/config.js
+++ src/service/config.js
@@ -1,1 +1,3 @@
-export const API_URL = "http://localhost:3100/api";
+//export const API_URL = "http://localhost:3100/api";
+export const API_URL =  process.env.NODE_ENV === 'development' ? "http://localhost:3100/api" : "https://chef-backend-pi.vercel.app/api";
+
